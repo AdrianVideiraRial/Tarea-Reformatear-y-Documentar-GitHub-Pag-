919b58ec56b722d23d716338cfa3c638c65b3663
Adrian <114355413+AdrianVideiraRial@users.noreply.github.com>
2022-11-23 20:26:22 +0100

    Primer commit

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ReformateoYJavaDoc","l":"Clase_Main"},{"p":"<Unnamed>","l":"Main"},{"p":"ReformateoYJavaDoc","l":"Perro"}];updateSearchResults();
